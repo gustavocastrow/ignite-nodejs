81fe1999ac5a477c697792261153dd32d12ec36b
Gustavo Castro <gustavocastrocs@gmail.com>
2021-08-09 09:21:35 -0300

    Criando saque na conta

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -1,6 +1,6 @@
-const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+
 const app = express();
 
 app.use(express.json());
@@ -8,7 +8,6 @@ app.use(express.json());
 const customers = [];
 
 //Middleware
-
 function verifyIfExistsAccountCPF(request, response, next){
   const { cpf } = request.headers;
   const customer = customers.find((customer) => customer.cpf === cpf);
@@ -18,6 +17,18 @@ function verifyIfExistsAccountCPF(request, response, next){
   }
   request.customer = customer;
   return next();
+}
+
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount;
+    }else {
+      return acc - operation.amount;
+    }
+  }, 0)
+
+  return balance;
 }
 
 /*
@@ -53,6 +64,44 @@ app.post('/account', (request, response) => {
 app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.json(customer.statement);
+});
+
+//Realizando um desposito
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+  return response.status(201).send();
+});
+
+//Realizando um saque 
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount){
+    return response.status(400).json({error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);

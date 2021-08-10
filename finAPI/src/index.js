6d36547bd48452d99c62de920ec3fff3b9a54e02
Gustavo Castro <gustavocastrocs@gmail.com>
2021-08-10 09:17:23 -0300

    FinAPI

--- finAPI/src/index.js
+++ finAPI/src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -103,6 +104,57 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
 
   return response.status(201).send();
 });
+
+//Busca extrato bancario por data
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  //formatando data
+  const dateFormat = new Date(date + " 00:00");
+  
+  const statement = customer.statement.filter((statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statement);
+});
+
+//Atualizando a conta
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+//Obtendo os dados da conta
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+//deletando account
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  //splice
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+//Retornando o balance 
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+});
+
 
 app.listen(3333);
 

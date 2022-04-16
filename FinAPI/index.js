e4ca414444b034834156a6eb97a414612e58c27f
Gustavo Castro <gustavocastrocs@gmail.com>
2022-04-15 21:22:28 -0300

    Listando Extrato

--- FinAPI/index.js
+++ FinAPI/index.js
@@ -14,10 +14,7 @@ app.use(express.json());
 
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
-
-    const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf === cpf
-    );
+    const customerAlreadyExists = customers.some((customer) => customer.cpf = cpf);
 
     if(customerAlreadyExists){
         return response.status(400).json({error: "Customers already exists!"});
@@ -32,6 +29,17 @@ app.post("/account", (request, response) => {
 
     return response.status(201).send();
 });
+
+//Buscando extrato bancario de um cliente
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    //const customer = customers.find((customer) => customer.cpf == cpf);
+    const customer = customers.find((customer) => customer.cpf === parseInt(cpf));
+
+    return response.json(customer.statement);
+});
+
 
 
 app.listen(3333);
